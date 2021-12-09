d6d667c4a14d4fb954379c18d9ff318826d61dbb
dev-yakuza <dev.yakuza@gmail.com>
2021-12-09 09:54:17 +0900

    Change channel example

---
+++ channel/6.unidirectional_channel/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+	var wg sync.WaitGroup
+	ch := make(chan int)
+
+	wg.Add(1)
+	go square(&wg, ch)
+
+	for i := 0; i < 10; i++ {
+		ch <- i * 2
+	}
+	close(ch)
+
+	wg.Wait()
+}
+
+func square(wg *sync.WaitGroup, ch <-chan int) {
+	for n := range ch {
+		fmt.Println("Square:", n*n)
+		time.Sleep(time.Second)
+	}
+	fmt.Println("Done")
+	wg.Done()
+}
