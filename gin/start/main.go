64c3e34b77dda3c5e657d18d8cde6de9c94b7a8a
dev-yakuza <dev.yakuza@gmail.com>
2022-04-07 11:22:53 +0900

    Add Gin web framework example

---
+++ gin/start/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func setupRouter() *gin.Engine {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(http.StatusOK, "pong")
+	})
+	return r
+}
+
+func main() {
+	r := setupRouter()
+	r.Run(":8080")
+}
